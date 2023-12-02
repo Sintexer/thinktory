e2f328ac925680e8af3a4510032049b8e735c8cb
Ilya Buglakov <ilboogl@gmail.com>
2023-12-02 14:27:15 +0300

    chore: remove unused classes

--- thinktory-service/src/main/kotlin/com/mibe/thinktory/service/book/BookRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.mibe.thinktory.service.book
-
-import org.bson.types.ObjectId
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface BookRepository : MongoRepository<Book, ObjectId> {
-    fun findByTelegramId(telegramId: Long): Book?
-}
