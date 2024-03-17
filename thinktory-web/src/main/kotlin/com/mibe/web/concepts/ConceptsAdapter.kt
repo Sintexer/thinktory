37fbbaa9d099af4c48ad55521586b0f6429f4ad6
Ilya Buglakov <ilboogl@gmail.com>
2024-03-17 16:07:46 +0300

    feat(concepts): add response Location header for create request

--- thinktory-web/src/main/kotlin/com/mibe/web/concepts/ConceptsAdapter.kt
+++ thinktory-web/src/main/kotlin/com/mibe/web/concepts/ConceptsAdapter.kt
@@ -4,9 +4,9 @@ import com.mibe.thinktory.service.concept.ConceptService
 import com.mibe.web.apis.ConceptsApiDelegate
 import com.mibe.web.models.ConceptCreateRequest
 import com.mibe.web.models.ConceptModel
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.net.URI
 
 @Service
 class ConceptsAdapter(private val conceptsService: ConceptService) : ConceptsApiDelegate {
@@ -14,7 +14,7 @@ class ConceptsAdapter(private val conceptsService: ConceptService) : ConceptsApi
     override fun createConcept(conceptCreateRequest: ConceptCreateRequest): ResponseEntity<ConceptModel> {
         val concept = conceptsService.createConcept(conceptCreateRequest.toCommand())
         return ResponseEntity
-            .status(HttpStatus.CREATED)
+            .created(URI.create("/concepts/${concept.id}"))
             .body(concept.toModel())
     }
 
